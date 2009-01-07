e542a874a88b3b413ed84c00b058e97f2054788e
Alan Gutierrez <alan@blogometer.com>
2009-01-07 17:47:49 -0600

    - Created a Bag unit test.

---
+++ src/test/java/com/goodworkalan/memento/BagTest.java
@@ -0,0 +1,12 @@
+package com.goodworkalan.memento;
+
+import org.junit.Test;
+
+public class BagTest
+{
+    @Test
+    public void constructor()
+    {
+        new Bag((long) 1, (long) 1, "Hello, World!");
+    }
+}
